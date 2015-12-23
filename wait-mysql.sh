6162152cfe7aeb4d5810068aed68b0797127d8f0
Tim Bennett <bennetimo@gmail.com>
2015-12-23 15:45:18 +0000

    make wait configurable by environment variables

--- wait-mysql.sh
+++ wait-mysql.sh
@@ -4,18 +4,16 @@
 
 echo "waiting for mysql to be available..."
 
-MYSQL_LOOPS="10"
-
 #wait for mysql
 i=0
 while ! nc $MYSQL_HOST $MYSQL_PORT >/dev/null 2>&1 < /dev/null; do
   i=`expr $i + 1`
-  if [ $i -ge $MYSQL_LOOPS ]; then
+  if [ $i -ge $WAIT_LOOPS ]; then
     echo "$(date) - ${MYSQL_HOST}:${MYSQL_PORT} still not reachable, giving up"
     exit 1
   fi
   echo "$(date) - waiting for ${MYSQL_HOST}:${MYSQL_PORT}..."
-  sleep 1
+  sleep $WAIT_SLEEP
 done
 
 echo "mysql is now available...continuing..."
