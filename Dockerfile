350fc56bb2d76e98bd8e62a44216bfc9a3c38633
Tim Bennett <bennetimo@gmail.com>
2015-12-23 01:45:04 +0000

    fix env var

--- Dockerfile
+++ Dockerfile
@@ -12,6 +12,7 @@ RUN apt-get clean && rm -rf /var/lib/apt/lists/* /tmp/* /var/tmp/*
 COPY wait-mysql.sh /wait-mysql.sh
 RUN chmod +x /wait-mysql.sh
 
+ENV MYSQL_HOST=mysql
 ENV MYSQL_PORT=3306
 
 ENTRYPOINT ["/wait-mysql.sh"]
--- wait-mysql.sh
+++ wait-mysql.sh
@@ -8,13 +8,13 @@ MYSQL_LOOPS="10"
 
 #wait for mysql
 i=0
-while ! nc $MYSQL_ENV_DB_HOST $MYSQL_PORT >/dev/null 2>&1 < /dev/null; do
+while ! nc $MYSQL_HOST $MYSQL_PORT >/dev/null 2>&1 < /dev/null; do
   i=`expr $i + 1`
   if [ $i -ge $MYSQL_LOOPS ]; then
-    echo "$(date) - ${MYSQL_ENV_DB_HOST}:${MYSQL_PORT} still not reachable, giving up"
+    echo "$(date) - ${MYSQL_HOST}:${MYSQL_PORT} still not reachable, giving up"
     exit 1
   fi
-  echo "$(date) - waiting for ${MYSQL_ENV_DB_HOST}:${MYSQL_PORT}..."
+  echo "$(date) - waiting for ${MYSQL_HOST}:${MYSQL_PORT}..."
   sleep 1
 done
 
